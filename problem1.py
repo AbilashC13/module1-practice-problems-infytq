8145e8a4ec5f0acb0899eb7da820a1fabb5fba03
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-05-25 09:30:44 +0530

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,21 @@
+#PF-Prac-1
+'''
+Write a python function to add 'ing' at the end of a given string and return the new string. 
+If the given string already ends with 'ing' then add 'ly'.
+If the length of the given string is less than 3, leave it unchanged.
+'''
+def add_string(str1):
+    #start writing your code here
+    if(len(str1)<3):
+        str1=str1
+    elif(st1.endswith("ing"):
+        str1=str1+"ly"
+    else:
+        str1=str1+"ing"
+
+
+  
+  return str1
+
+str1="com"
+print(add_string(str1))
