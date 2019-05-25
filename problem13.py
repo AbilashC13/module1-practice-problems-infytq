20bddb60ffe1c3c828e2740ef9c770e2a6d73402
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-05-25 16:30:39 +0530

    Update problem13.py

--- problem13.py
+++ problem13.py
@@ -1,4 +1,12 @@
 #PF-Prac-13
+'''
+Write a python function which accepts three numbers and returns True if
+a.	one of the three numbers is "close" to any one of the other numbers (differing from a number by at most 1), and
+
+b.Number that is left out is "far", differing from both other values by 2 or more.
+
+Return false if the above mentioned conditions are not satisfied.
+'''
 import math
 
 def close_number(num1,num2,num3):
