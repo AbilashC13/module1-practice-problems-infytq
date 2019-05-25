455d6d8d58c4aa53565851e8ada854590d763576
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-05-25 14:54:56 +0530

    Update problem29.py

--- problem29.py
+++ problem29.py
@@ -1,6 +1,7 @@
 #PF-Prac-29
 '''
-Given a list of numbers, write a python function to exchange the first n/2 elements of a list with the last n/2 elements. The function should return the new list.
+Given a list of numbers, write a python function to exchange the first n/2 elements of a list with the last n/2 elements.
+The function should return the new list.
 n represents the number of elements in the list. Assume that it will always be even.
 '''
 def exchange_list(number_list):
